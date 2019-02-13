2fb4b5e71215c79fab39572445ab0888c30a187e
gregology <greg@gho.st>
2019-02-13 16:17:39 -0500

    fixing live videos and prettier settings

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -12,13 +12,19 @@ class Channel < ApplicationRecord
 
   def get_videos
     yt_channel.videos.each do |yt_video|
-      self.videos.where(yt_id: yt_video.id).first_or_create do |video|
-        video.yt_id        = yt_video.id
-        video.title        = yt_video.title
-        video.description  = yt_video.description
-        video.tags         = yt_video.tags
-        video.published_at = yt_video.published_at
-        video.duration     = yt_video.duration
+      begin
+        if yt_video.length.to_i > 0
+          self.videos.where(yt_id: yt_video.id).first_or_create do |video|
+            video.yt_id        = yt_video.id
+            video.title        = yt_video.title
+            video.description  = yt_video.description
+            video.tags         = yt_video.tags
+            video.published_at = yt_video.published_at
+            video.duration     = yt_video.duration
+          end
+        end
+      rescue Yt::Errors::NoItems
+        # ignore live videos that fail
       end
     end
   end
