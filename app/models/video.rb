7717f57efa7a4bf488a995b579508c8f11218f1a
gregology <greg@gho.st>
2019-03-05 13:16:38 -0500

    validating uniquiness for yt_id

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,5 @@
 class Video < ApplicationRecord
   belongs_to :channel
   belongs_to :previous_video, class_name: 'Video', foreign_key: 'previous_video_id', optional: true
+  validates :yt_id, uniqueness: true
 end
